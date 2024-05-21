af5a96f0d8f370b13ca4410cd53d62c65a9ec068
joshuasaetern <JoshuaSaetern17@gmail.com>
2024-05-21 13:34:57 -0700

    Changed MainWindow.xaml.cs and MainWindow.xaml

--- Lecture9/MainWindow.xaml.cs
+++ Lecture9/MainWindow.xaml.cs
@@ -19,6 +19,38 @@ namespace Lecture9
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void btnPractice_Click(object sender, RoutedEventArgs e)
+        {
+            lblStatus.Content = ($"CheckBox1: {chkBox1.IsChecked}\nCheckBox2 {chkBox2.IsChecked}\nRadioButton1: {radioBtn1.IsChecked}\nRadioButton2: {radioBtn2.IsChecked}");
+        }
+
+        private void btnPart2_Click(object sender, RoutedEventArgs e)
+        {
+            Order order = new Order(txtBoxName.Text, chkBoxOvernightDelivery.IsChecked == true, chkBoxPerishable.IsChecked == true);
+            lblStatus.Content = order.ToString();
+        }
+
+        private void radioButtonBlue_Checked(object sender, RoutedEventArgs e)
+        {
+            gridMain.Background = new SolidColorBrush(Colors.LightBlue);
+        }
+
+        private void radioButtonRed_Checked(object sender, RoutedEventArgs e)
+        {
+            gridMain.Background = new SolidColorBrush(Colors.LightPink);
+        }
+
+        private void radioButtonGreen_Checked(object sender, RoutedEventArgs e)
+        {
+            gridMain.Background = new SolidColorBrush(Colors.LightGreen);
+        }
+
+        private void radioButtonYellow_Checked(object sender, RoutedEventArgs e)
+        {
+            gridMain.Background = new SolidColorBrush(Colors.LightGoldenrodYellow);
         }
     }
 }
---
+++ Lecture9/Order.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture9
+{
+    internal class Order
+    {
+        //Fields
+        String orderName;
+        bool isOverNight;
+        bool isPerishable;
+
+        //Constructors
+        public Order(String orderName, bool isOverNight, bool isPerishable)
+        {
+            this.orderName = orderName;
+            this.isOverNight = isOverNight;
+            this.isPerishable = isPerishable;
+        }
+
+        //Properties
+        public string OrderName { get => orderName; set => orderName = value; }
+        public bool IsOverNight { get => isOverNight; set => isOverNight = value; }
+        public bool IsPerishable { get => isPerishable; set => isPerishable = value; }
+
+        //Methods
+        public override String ToString()
+        {
+            return $"Name: {this.orderName}\nOvernight delivery: {this.isOverNight}\nPerishable: {this.isPerishable}";
+        }
+
+    }
+}
